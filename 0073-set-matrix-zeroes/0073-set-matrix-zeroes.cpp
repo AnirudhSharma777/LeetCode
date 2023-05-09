dd4350ba45ba35f13fddca0a02459f8aedd69aa1
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2023-05-09 11:27:03 +0530

    Time: 21 ms (16.28%), Space: 13.3 MB (73.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,49 +1,61 @@
-// let try brute force first 
+// LET TRY BETTER THE BRUTE FORCE APPROACH
 
-// TC : O(n^3) 
+// In this approach i am using Extra space o(n+m);
+// but the time complexity is O(n*m) which is better than brute force approach
+
+// let optimize more neglet using  extra space 
+//  means space complexity is O(1)
+
 
 class Solution {
 public:
-    
-    
-    void mark_Row(vector<vector<int>>& matrix, int n,int m,int i){
-        
-        for(int j = 0; j<m; j++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = INT_MIN+1 ;
-            }
-        }
-    }
-    
-    void mark_col(vector<vector<int>>& matrix,int n, int m, int j){
-        
-        for(int i = 0; i<n; i++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = INT_MIN+1;
-            }
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
         int m = matrix[0].size();
         
+//         int row[n] = {0};  matrix[..][0]
+//         int col[m] = {0};  matrix[0][..]
+        
+        int col0 = 1;
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    mark_Row(matrix,n,m,i);
-                    mark_col(matrix,n,m,j);
+                    matrix[i][0] = 0;
+                   
+                    if(j != 0){
+                         matrix[0][j] = 0;
+                    }
+                    else{
+                        col0 = 0;
+                    }
                 }
             }
         }
         
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][j] != 0){
+                    
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
         
-        for(int i = 0; i<n; i++){
+        if(matrix[0][0] == 0){
             for(int j = 0; j<m; j++){
-                if(matrix[i][j] == INT_MIN+1){
-                    matrix[i][j] = 0;
-                }
+                matrix[0][j] = 0;
             }
         }
+        
+        if(col0 == 0){
+            for(int i = 0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
+
     }
 };
