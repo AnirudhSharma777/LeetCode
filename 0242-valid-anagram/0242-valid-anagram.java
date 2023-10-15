d18cde1b3c156d742cf5d0dc40594ff69e49eb20
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2023-10-15 13:01:50 +0530

    Time: 2 ms (98.59%), Space: 43.9 MB (16.93%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+         if(s.length() != t.length()){
+            return false;
+        }
+        
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        
+        for(char ch : t.toCharArray()){
+            freq[ch - 'a']--;
+        }
+                          
+        for(int it : freq){
+            if(it != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
