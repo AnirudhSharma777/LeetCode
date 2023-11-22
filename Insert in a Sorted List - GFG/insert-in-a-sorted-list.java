429695bc54b5eb48a4726fbea88f030abbd2b6a4
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2023-11-22 11:28:57 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head, int key) {
+        // Add your code here.
+        if(head == null){
+            Node newNode = new Node(key);
+            return newNode;
+        }
+        Node temp = head;
+        if(temp.data > key){
+            Node newNode = new Node(key);
+            newNode.next = head;
+            head = newNode;
+        }
+        else{
+            Node newNode = new Node(key);
+            Node prev = head;
+            while(temp != null && temp.data < key){
+                prev = temp;
+                temp = temp.next;
+            }
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+        return head;
+    }
+}
