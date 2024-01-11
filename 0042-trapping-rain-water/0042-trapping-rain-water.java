976b4d5e70ecd05652af49e7170f1d2b4cf843f0
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-01-11 16:31:03 +0530

    Time: 1 ms (77.89%), Space: 45.8 MB (30.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,25 @@
-
 class Solution {
-    public int trap(int[] height) { // Tc : O(n) & Sc : O(n)
-        Stack<Integer> st = new Stack<>();
-        int n = height.length,ans = 0;
+    public int trap(int[] height) {
+        int n = height.length; 
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int water = 0;
+        
         for(int i = 0; i<n; i++){
-            while(!st.isEmpty() && height[i] > height[st.peek()]){
-                int cur = st.peek();
-                st.pop();
-                if(st.isEmpty()){
-                    break;
-                }
-                int diff = i - st.peek() - 1;
-                ans += (Math.min(height[st.peek()],height[i]) - height[cur])*diff;
+            if(i==0){
+                left[0] = height[0];
+                right[n-1] = height[n-1];
             }
-            st.push(i);
+            else{
+                left[i] = Math.max(left[i-1],height[i]);
+                right[n-i-1] = Math.max(right[n-i],height[n-i-1]);
+            }
         }
-        return ans;
+        
+        for(int i = 0; i<n; i++){
+            water += Math.min(left[i],right[i]) - height[i];
+        }
+        return water;
+        
     }
 }
