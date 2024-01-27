33d170b08e4dc800e9f4705a8b5a0232831ef5e8
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-01-27 23:09:08 +0530

    Time: 1 ms (100.00%), Space: 44.5 MB (24.37%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,32 +10,30 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        int carry = 0;
-        while(l1 != null || l2 != null){
-            int sum = 0;
-            if(l1 != null){
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            
-            if(l2 != null){
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            
-            sum += carry;
-            carry = sum / 10;
-            ListNode node = new ListNode(sum % 10);
-            temp.next = node;
-            temp = temp.next;
-        }
-        
-        if(carry != 0){
-            temp.next = new ListNode(carry);
-        }
-        return dummy.next;
+        ListNode l3 = new ListNode(0);
+        recursiveAdd(l1, l2, l3, 0);
+        return l3;
     }
+    
+     private void recursiveAdd(ListNode l1, ListNode l2, ListNode l3, int carryOver){
+        //calculate value of the current digit
+        l3.val = ((l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carryOver) % 10;
+
+        //calculate carry over to the next digit
+        int carryOverNew = ((l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carryOver) / 10;
+
+        //take the next digit from the two operands
+        if (l1 != null) l1 = l1.next;
+        if (l2 != null) l2 = l2.next;
+
+        //another digit is only needed if at least one these are true:
+        //1. the first operand has another digit
+        //2. the second operand has another digit
+        //3. the carry over is more than zero
+        if (l1 != null || l2 != null || carryOverNew > 0) {
+            //only create another digit when it is needed
+            l3.next = new ListNode(0);
+            recursiveAdd(l1, l2, l3.next, carryOverNew);
+        }
+     }
 }
