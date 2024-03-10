c55fd13ba624747ac2e98a39045506ba7fddfd93
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-03-10 17:14:23 +0530

    Time: 24 ms (79.19%), Space: 46.2 MB (74.89%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -8,12 +8,14 @@ class Solution {
         hm.put(0,1);
         for(int i = 0; i<n; i++){
             sum += nums[i];
-                       
+                  
+            
             if(hm.containsKey(sum-k)){
                 cnt += hm.get(sum - k);
             }
             
             hm.put(sum,hm.getOrDefault(sum,0)+1);
+           
         }
         return cnt;
     }
