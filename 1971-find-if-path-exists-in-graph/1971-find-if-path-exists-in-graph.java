04176fe47f9e283978bc9767c44bf588f4b48273
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-10 10:42:56 +0530

    Time: 140 ms (37.51%), Space: 177.7 MB (25.02%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,45 +1,84 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
+    
+    // bfs
+//    public boolean validPath(int n, int[][] edges, int source, int destination) {
         
         
-        HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
+//         HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
         
+//         for(int i = 0; i<n; i++){
+//             graph.put(i,new ArrayList<>());
+//         }
+        
+//         for(int[] edge : edges){
+//             int u = edge[0];
+//             int v = edge[1];
+            
+//             graph.get(u).add(v);
+//             graph.get(v).add(u);
+//         }
+        
+//         // System.out.println(graph);
+        
+//         boolean[] visited = new boolean[n];
+//         Queue<Integer> queue = new LinkedList<>();
+//         queue.offer(source);
+//         visited[source] = true;
+        
+//         while(!queue.isEmpty()){
+//             int node = queue.peek();
+//             queue.remove();
+//             if(source == destination){
+//                 return true;
+//             }
+            
+//             for(Integer nbr : graph.get(node)){
+//                 if(!visited[nbr]){
+//                     queue.offer(nbr);
+//                     visited[nbr] = true;
+//                 }
+//             }
+//         }
+        
+        
+//         return visited[destination] ? true : false;
+        
+//     }
+    
+//     dfs
+    public boolean validPath(int n, int[][] edges, int source, int destination){
+        
+        ArrayList<Integer>[] adj = new ArrayList[n];
         for(int i = 0; i<n; i++){
-            graph.put(i,new ArrayList<>());
+            adj[i] = new ArrayList<>();
         }
         
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
             
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            adj[u].add(v);
+            adj[v].add(u);
         }
-        
-        // System.out.println(graph);
         
         boolean[] visited = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(source);
+        
+        return dfs(source,destination,visited,adj);
+    } 
+
+    private boolean dfs(int source,int destination,boolean[] visited,ArrayList<Integer>[] adj){
+        
         visited[source] = true;
         
-        while(!queue.isEmpty()){
-            int node = queue.peek();
-            queue.remove();
-            if(source == destination){
-                return true;
-            }
-            
-            for(Integer nbr : graph.get(node)){
-                if(!visited[nbr]){
-                    queue.offer(nbr);
-                    visited[nbr] = true;
-                }
-            }
+        if(source == destination){
+            return true;
         }
         
-        
-        return visited[destination] ? true : false;
-        
+        for(Integer nbr : adj[source]){
+            if(!visited[nbr] && dfs(nbr,destination,visited,adj)){
+                return true;
+            }
+        }
+        return false;
     }
 }
