11e8b6de52c4e2cc02d1675106ca6d658485e98e
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-11 22:02:48 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (40.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,36 @@
 class Solution {
-    public int climbStairs(int n) {
+    
+    // tc : O(2^n)
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return helper(n,dp);
+//     }
+    
+//     private int helper(int n,int[] dp){
+//         if(n == 0 || n == 1){
+//             return 1;
+//         }
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+//         dp[n] = helper(n-1,dp) + helper(n-2,dp);
+//         return dp[n];
+//     }
+    
+    
+    // iterative tc: O(n)
+    public int climbStairs(int n){
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return helper(n,dp);
-    }
-    
-    private int helper(int n,int[] dp){
-        if(n == 0 || n == 1){
-            return 1;
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = helper(n-1,dp) + helper(n-2,dp);
+        
         return dp[n];
+        
     }
-}    
-    
+}   
