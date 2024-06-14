d212533b79656ec3715478714da9b5dfb305e03b
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-13 22:17:21 -0700

    Update count-ways-to-reach-the-nth-stair.java

--- Medium/Count ways to reach the n'th stair/count-ways-to-reach-the-nth-stair.java
+++ Medium/Count ways to reach the n'th stair/count-ways-to-reach-the-nth-stair.java
@@ -31,6 +31,8 @@ class GFG {
 // } Driver Code Ends
 
 
+
+
 class Solution
 {
     static int mod = 1000000007;
@@ -51,26 +53,54 @@ class Solution
     
     // Using Recursion + Memoization
     // Tc: O(n) & sc : O(n)
-    int countWays(int n){
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
-    }
+    // int countWays(int n){
+    //     int[] dp = new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return helper(n,dp);
+    // }
     
-    int helper(int n,int[] dp){
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
+    // int helper(int n,int[] dp){
+    //     if(n == 0 || n == 1){
+    //         return 1;
+    //     }
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
         
-        dp[n] = (helper(n-1,dp) + helper(n-2,dp)) % mod;
+    //     dp[n] = (helper(n-1,dp) + helper(n-2,dp)) % mod;
         
-        return dp[n];
-    }
+    //     return dp[n];
+    // }
     
     // using tabulation
+    // Tc: O(n) & sc : O(n)
+    // int countWays(int n){
+    //     int[] dp = new int[n+1];
+    //     dp[0] = 1;
+    //     dp[1] = 1;
+        
+    //     for(int i = 2; i<=n; i++){
+    //         dp[i] = (dp[i-1] + dp[i-2]) % mod;
+    //     }
+    //     return dp[n];
+    // }
+    
+    
+    // space optimization
+    // tc: O(n) & now sc : O(1)
+    int countWays(int n){
+        
+        int prev1 = 1;
+        int prev2 = 1;
+        
+        for(int i = 2; i<=n; i++){
+            int climb = (prev1 + prev2) % mod;
+            prev1 = prev2;
+            prev2 = climb;
+        }
+        return prev2;
+    }
+    
+    
     
 }
-
