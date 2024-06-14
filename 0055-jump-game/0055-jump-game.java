814c4cbb7f174a2fcd13d2bc60599fa5e95907d2
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-14 04:53:48 -0700

    Time: 90 ms (15.54%), Space: 45.5 MB (46.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,26 +1,44 @@
 class Solution {
-    public boolean canJump(int[] nums) {
+    // using recursion + memoization
+    // Tc: O(n^2) & sc:O(n)
+//     public boolean canJump(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return jumpFrom(0,nums,dp);
+//     }
+    
+//    public static boolean jumpFrom(int index , int[] nums,int[] dp){
+//         if(index>=nums.length-1)
+//           return true;
+
+//         if(dp[index]!=-1)
+//          return dp[index]==0?false:true;
+        
+//         for(int i=1;i<=nums[index];i++){
+//             if(jumpFrom(index + i , nums,dp)==true)
+//             { dp[index]=1;
+//               return true;
+//             }
+//         }
+
+//         dp[index]=0;
+//         return false;
+//     }
+    
+    
+    public boolean canJump(int[] nums){
         int n = nums.length;
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return jumpFrom(0,nums,dp);
-    }
-    
-   public static boolean jumpFrom(int index , int[] nums,int[] dp){
-        if(index>=nums.length-1)
-          return true;
-
-        if(dp[index]!=-1)
-         return dp[index]==0?false:true;
-        
-        for(int i=1;i<=nums[index];i++){
-            if(jumpFrom(index + i , nums,dp)==true)
-            { dp[index]=1;
-              return true;
+        dp[n-1] = 1;
+        for(int i = n-2; i>=0; i--){
+            for(int j = 1; j<=nums[i] && i+j<n; j++){
+                if(dp[i+j] == 1){
+                    dp[i] = 1;
+                    break;
+                }
             }
         }
-
-        dp[index]=0;
-        return false;
+        return dp[0] == 1 ? true : false;
     }
 }
