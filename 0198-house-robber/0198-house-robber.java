bceb4014cdf08106c8c2e1becbbb1c8ad633c8e4
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-15 03:51:03 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (48.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,54 @@
 class Solution {
-    public int rob(int[] nums) {
+    
+    // using recursion
+    // tc :O(2^n) & sc :O(n)
+    
+    // using recursion + memoization
+    // tc :O(n) & sc : O(n)
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+//         Arrays.fill(dp,-1);
+//         return helper(nums,n-1,dp);
+//     }
+    
+//     private int helper(int[] nums,int idx,int[] dp){
+//         if(idx == 0){
+//             return nums[idx];
+//         }
+//         if(idx < 0){
+//             return 0;
+//         }
+        
+//         if(dp[idx] != -1){
+//             return dp[idx];
+//         }
+//         int skip = helper(nums,idx-1,dp);
+//         int take = nums[idx] + helper(nums,idx-2,dp);
+        
+//         dp[idx] = Math.max(skip,take);
+//         return dp[idx];
+//     }
+    
+    public int rob(int[] nums){
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return helper(nums,n-1,dp);
-    }
-    
-    private int helper(int[] nums,int idx,int[] dp){
-        if(idx == 0){
-            return nums[idx];
-        }
-        if(idx < 0){
-            return 0;
+        
+        dp[0] = nums[0];
+        int neg = 0;
+        
+        for(int i = 1; i<n; i++){
+            
+            int skip = 0 + dp[i-1];
+            int take = nums[i];
+            if(i > 1){
+                take += dp[i-2];
+            }                
+            
+            dp[i] = Math.max(skip,take);
         }
         
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int skip = helper(nums,idx-1,dp);
-        int take = nums[idx] + helper(nums,idx-2,dp);
+        return dp[n-1];
         
-        dp[idx] = Math.max(skip,take);
-        return dp[idx];
     }
 }
