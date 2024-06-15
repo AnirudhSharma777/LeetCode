140ab106f9ff33563c29b3a31b327c2279046212
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-15 16:52:19 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (48.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -30,25 +30,53 @@ class Solution {
 //         return dp[idx];
 //     }
     
+    
+    // using tabulation 
+    // tc:O(n) & sc:O(n)
+//     public int rob(int[] nums){
+//         int n = nums.length;
+//         int[] dp = new int[n];
+        
+//         dp[0] = nums[0];
+//         
+        
+//         for(int i = 1; i<n; i++){
+            
+//             int skip = 0 + dp[i-1];
+//             int take = nums[i];
+//             if(i > 1){
+//                 take += dp[i-2];
+//             }                
+            
+//             dp[i] = Math.max(skip,take);
+//         }
+        
+//         return dp[n-1];
+        
+//     }
+    
+    
+    // space optimization
+    // Tc: O(n) & sc:O(1)
+    
     public int rob(int[] nums){
         int n = nums.length;
-        int[] dp = new int[n];
-        
-        dp[0] = nums[0];
-        int neg = 0;
-        
+        int prev1 = nums[0];
+        int prev2 = 0;
+        int cur = 0;
         for(int i = 1; i<n; i++){
-            
-            int skip = 0 + dp[i-1];
+        
             int take = nums[i];
             if(i > 1){
-                take += dp[i-2];
-            }                
+                take += prev2;
+            }
+            int skip = 0 + prev1;
+            cur = Math.max(skip,take);
+            prev2 = prev1;
+            prev1 = cur;
             
-            dp[i] = Math.max(skip,take);
         }
         
-        return dp[n-1];
-        
+        return prev1;
     }
 }
