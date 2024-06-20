c96ab78be19f9b6fce7170825c25eb3056fe597e
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-20 13:00:47 +0530

    Time: 5 ms (33.93%), Space: 44 MB (59.48%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -18,13 +18,30 @@ class Solution {
         
         return dp[i][j];
     }
+    
+    
+    // tabulation
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         
         int[][] dp = new int[n+1][n+1];
         for(int[] it : dp){
-            Arrays.fill(it,-1);
+            Arrays.fill(it,0);
         }
-        return recursion(0,0,triangle,n,dp);
+        
+        for(int i = 0; i<n; i++){
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
+        
+        for(int i = n-2; i>=0; i--){
+            for(int j = i; j>=0; j--){
+                 int d = triangle.get(i).get(j) + dp[i+1][j];
+                 int dg = triangle.get(i).get(j) + dp[i+1][j+1];
+                 
+                dp[i][j] = Math.min(d,dg);
+            }
+        }
+        
+        return dp[0][0];
     }
 }
