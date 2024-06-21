839f449037337d20907e238b3f50d65f731fe167
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-21 14:54:05 +0530

    Time: 30 ms (62.69%), Space: 51 MB (60.36%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -51,8 +51,6 @@ class Solution {
         
         for(int i = 1; i<n; i++){
             for(int j = 0; j<n; j++){
-                dp[i][j] = grid[i][j];
-                
                 
                  int min = Integer.MAX_VALUE;
                 for(int k = 0; k<n; k++){
@@ -60,7 +58,7 @@ class Solution {
                         min = Math.min(min,dp[i-1][k]);
                     }
                 }
-                dp[i][j] += min;
+                dp[i][j] = grid[i][j] + min;
             }
         }
         
