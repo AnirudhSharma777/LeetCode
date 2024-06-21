7e5b9f533fd9b3758b27d4a3a84aae066ba5ea17
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-21 14:42:04 +0530

    Time: 189 ms (7.49%), Space: 51.4 MB (30.15%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int[] it : dp){
+            Arrays.fill(it,-1);
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<n; i++){
+            min = Math.min(min,recursion(0,i,n,grid,dp));
+        }
+        return min;
+    }
+    
+    public int recursion(int i,int j,int n,int[][] grid,int[][] dp){
+        
+        if(i >= n || j >= n){
+            return 0;
+        }
+        if(i == n-1){
+            return grid[i][j];
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int min = Integer.MAX_VALUE;
+        for(int k = 0; k<n; k++){
+            if(k != j){
+                min = (int)Math.min(min ,grid[i][j]+ recursion(i+1,k,n,grid,dp));
+            }
+        }
+        
+        return dp[i][j] = min;
+    }
+}
