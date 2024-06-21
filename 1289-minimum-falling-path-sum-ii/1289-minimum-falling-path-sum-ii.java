b5c83ef5a7b01c92c783c167c99a94e5f15b7c49
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-06-21 14:52:53 +0530

    Time: 30 ms (62.69%), Space: 51.9 MB (7.53%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,36 +1,73 @@
 class Solution {
+    
+    // recursion + memoization
+    
+//     public int minFallingPathSum(int[][] grid) {
+//         int n = grid.length;
+//         int[][] dp = new int[n+1][n+1];
+//         for(int[] it : dp){
+//             Arrays.fill(it,-1);
+//         }
+//         int min = Integer.MAX_VALUE;
+//         for(int i = 0; i<n; i++){
+//             min = Math.min(min,recursion(0,i,n,grid,dp));
+//         }
+//         return min;
+//     }
+    
+//     public int recursion(int i,int j,int n,int[][] grid,int[][] dp){
+        
+//         if(i >= n || j >= n){
+//             return 0;
+//         }
+//         if(i == n-1){
+//             return grid[i][j];
+//         }
+        
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+//         int min = Integer.MAX_VALUE;
+//         for(int k = 0; k<n; k++){
+//             if(k != j){
+//                 min = (int)Math.min(min ,grid[i][j]+ recursion(i+1,k,n,grid,dp));
+//             }
+//         }
+        
+//         return dp[i][j] = min;
+//     }
+    
+    
     public int minFallingPathSum(int[][] grid) {
         int n = grid.length;
         int[][] dp = new int[n+1][n+1];
         for(int[] it : dp){
-            Arrays.fill(it,-1);
+            Arrays.fill(it,0);
         }
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i<n; i++){
-            min = Math.min(min,recursion(0,i,n,grid,dp));
-        }
-        return min;
-    }
-    
-    public int recursion(int i,int j,int n,int[][] grid,int[][] dp){
-        
-        if(i >= n || j >= n){
-            return 0;
-        }
-        if(i == n-1){
-            return grid[i][j];
+       
+        for(int j = 0; j<n; j++){
+           dp[0][j] = grid[0][j];
         }
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int min = Integer.MAX_VALUE;
-        for(int k = 0; k<n; k++){
-            if(k != j){
-                min = (int)Math.min(min ,grid[i][j]+ recursion(i+1,k,n,grid,dp));
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<n; j++){
+                dp[i][j] = grid[i][j];
+                
+                
+                 int min = Integer.MAX_VALUE;
+                for(int k = 0; k<n; k++){
+                    if(k != j){
+                        min = Math.min(min,dp[i-1][k]);
+                    }
+                }
+                dp[i][j] += min;
             }
         }
         
-        return dp[i][j] = min;
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i<n; i++){
+            ans = Math.min(ans,dp[n-1][i]);
+        }
+        return ans;
     }
 }
