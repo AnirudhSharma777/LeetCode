aa36aef931a8fd95ef065af472b786bf9bb3f30c
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2024-10-21 08:02:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a k sorted doubly linked list/sort-a-k-sorted-doubly-linked-list.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class DLLNode {
+    int data;
+    DLLNode next;
+    DLLNode prev;
+
+    DLLNode(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
+
+public class Main {
+    public static void push(DLLNode tail, int new_data) {
+        DLLNode newNode = new DLLNode(new_data);
+        newNode.next = null;
+        newNode.prev = tail;
+
+        if (tail != null) {
+            tail.next = newNode;
+        }
+    }
+
+    public static void printList(DLLNode head) {
+        if (head == null) {
+            return;
+        }
+
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String[] arr = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(br.readLine().trim());
+
+            DLLNode head = new DLLNode(Integer.parseInt(arr[0]));
+            DLLNode tail = head;
+
+            for (int i = 1; i < arr.length; i++) {
+                push(tail, Integer.parseInt(arr[i]));
+                tail = tail.next;
+            }
+
+            Solution obj = new Solution();
+            DLLNode sorted_head = obj.sortAKSortedDLL(head, k);
+            printList(sorted_head);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public DLLNode sortAKSortedDLL(DLLNode head, int k) {
+        // Code here
+        
+        
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        DLLNode temp1 = head, temp2 = head;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        while(temp1 != null){
+            if(pq.size() == k+1){
+                temp2.data = pq.poll();
+                temp2 = temp2.next;
+            }
+            pq.add(temp1.data);
+            temp1 = temp1.next;
+        }
+        
+        while(!pq.isEmpty()){
+            temp2.data = pq.poll();
+            temp2 = temp2.next;
+        }
+        
+        return head;
+    }
+}
