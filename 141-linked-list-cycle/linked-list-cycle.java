826d84017634f35212dc344fe80615d7cdcd3e27
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-30 09:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (63.23%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -16,20 +16,22 @@ public class Solution {
             return false;
         }
 
-        if(head.next == head){
-            return true;
+        if(head.next == null){
+            return false;
         }
 
-        ListNode slow = head;
-        ListNode fast = head;
+        ListNode slow = head, fast = head;
 
         while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
+
             if(slow == fast){
                 return true;
             }
         }
+
         return false;
+        
     }
 }
