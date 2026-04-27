88719387b7b4cc591d48ed97ee9f8f0af00bbb21
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2026-04-27 15:57:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        // code here
+        int n = arr.length;
+        
+        if(n <= 1){
+            return 0;
+        }
+        
+        if(arr[0] == 0){
+            return -1;
+        }
+        
+        int maxReach = arr[0];
+        int jumps = 1;
+        int steps = arr[0];
+        
+        for(int i = 1; i<n-1; i++){
+            
+            maxReach = Math.max(maxReach,i+arr[i]);
+            
+            steps--;
+            
+            if(steps == 0){
+                jumps++;
+                
+                if(i >= maxReach){
+                    return -1;
+                }
+                
+                steps = maxReach - i;
+            }
+            
+        }
+        
+        return jumps;
+    }
+}
