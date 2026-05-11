b7faf1db91fbbd907356a414950ecd00d09c486b
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2026-05-11 10:58:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
@@ -7,14 +7,14 @@ class Solution {
         int i = n-1;
         int j = 0;
         
-        while(i>=0 && j<m) {
+        while(i>=0 && j<m){
             if(a[i] > b[j]){
                 
                 int temp = a[i];
                 a[i] = b[j];
                 b[j] = temp;
-                
             }
+            
             i--;
             j++;
         }
